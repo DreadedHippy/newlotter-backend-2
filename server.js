70a921baca7af155a4489d7720383c6fdf001db3
Aizee Onot <onotaizee@gmail.com>
2021-03-18 00:00:31 +0100

    Oauth integration

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = require('./backend/app')
 const debug = require('debug')('node-angular')
 const server = http.createServer(app);
 const mongoose = require('mongoose')
+
 port = process.env.PORT || 3000;
 
 app.set('port', port);
